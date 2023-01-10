9447381ef209283a16d8ea8d03a5928a15774511
Wiibleyde <nathan@bonnell.fr>
2023-01-10 11:43:13 +0100

    Update with script save

--- 6/2/tp6_backup.sh
+++ 6/2/tp6_backup.sh
@@ -2,7 +2,6 @@
 # Created : 2023-01-10 at 11am
 # Nathan
 # Simple script of backup for nextcloud
-fileName="nextcloud_'date +%Y%m%d%H%M%S'.tar.gz"
-rsync -Aazvx /var/www/tp5_nextcloud/ /srv/backup/$fileName
+fileName="nextcloud_$(date +%Y%m%d%H%M%S).tar.gz"
+tar -czf /srv/backup/$fileName /var/www/tp5_nextcloud/
 echo "Filename : $fileName saved in /srv/backup"
-
